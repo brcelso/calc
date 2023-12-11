ba59141988e8ec2529c36dd65bcf24ed369d032a
brcelso <129978598+brcelso@users.noreply.github.com>
2023-12-11 16:21:30 +0000

    calc

---
+++ app/calculator.js
@@ -0,0 +1,53 @@
+// components/Calculator.js
+import React, { useState } from 'react';
+import styles from './calculator.module.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Erro');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <input type="text" value={input} readOnly className={styles.input} />
+      <button onClick={() => handleButtonClick('7')} className={styles.button}>7</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>8</button>
+      <button onClick={() => handleButtonClick('7')} className={styles.button}>9</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>/</button>
+      <button onClick={() => handleButtonClick('7')} className={styles.button}>4</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>5</button>
+      <button onClick={() => handleButtonClick('7')} className={styles.button}>6</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>*</button>
+      <button onClick={() => handleButtonClick('7')} className={styles.button}>1</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>2</button>
+      <button onClick={() => handleButtonClick('8')} className={styles.button}>3</button>
+      <button onClick={() => handleButtonClick('-')} className={styles.button}>-</button>
+      <button onClick={() => handleButtonClick('0')} className={styles.button}>0</button>
+      <button onClick={() => handleButtonClick('.')} className={styles.button}>.</button>
+      <button onClick={() => handleButtonClick('=')} className={styles.button}>=</button>
+      <button onClick={() => handleButtonClick('+')} className={styles.button}>+</button>
+      <button onClick={() => handleButtonClick('C')} className={styles.button}>C</button>
+      
+      {/* ... rest of the buttons ... */}
+      <p className={styles.result}>Resultado: {result}</p>
+    </div>
+  );
+};
+
+export default Calculator;
+
+
